82b687780f6b28b1b3166e74a856d54a41cad1c2
Bhaskar <bhaskar@bhaskar-Dell-System-XPS-L502X.(none)>
2015-06-07 19:34:27 +0530

    added changed to driver module

--- driver.c
+++ driver.c
@@ -1,22 +1,72 @@
 //driver for testing search module
-//check search.h for availaible method declarations
+//check header files for method declarations
+//All the inputs are static right now (Edit code in switch cases to get input dynamically)
 
 
 #include "search.h"
 #include<stdio.h>
+#include<stdlib.h>
 
 main() {
+	//declaring variables
 	//for windows use drive paths like char * initPath="E:\\";//
-	char * initPath="/home/bhaskar/Documents/programming/summer_2015/"; //reference to start search
-	DIR *currentDirectory; //creating refernce to directory 
-	currentDirectory=opendir(initPath); 
+        char * initPath="/home/bhaskar/Documents/programming/summer_2015/"; //reference to start search
+        DIR *currentDirectory; //creating refernce to directory
 
-	//directory search (in case of exe need to look up for directory)
-//	printf("Searching directory\n");
-//	searchExe(currentDirectory,"idm",initPath); //start the search now
+	
+	
+	//using switch cases to perform tasks
+	int i;
+	
+	//perform tasks bases upon i values
+	/*
+		0. Exit the driver 
+		1. Searching Exe installation path
+		2. Searching for a particular file in some directory structure
+		3. Running a command through a spawned process and producing a file
+		4. Zip up the contents of the file using https://code.google.com/p/miniz/ library and ask user for password
+		5. Encrypt the file using http://www.cryptopp.com/ library
+	*/
+	
+	while(1) {
+		//priing choices on console
+        	printf("\n\n0. Exit the driver\n1. Searching Exe installation path\n2. Searching for a particular file in some directory structure\n3. Running a command through a spawned process and producing a file\n4. Zip up the contents of the file using https://code.google.com/p/miniz/ library and ask user for password\n5. Encrypt the file using http://www.cryptopp.com/ library\n\n\n");
+		 scanf("%d",&i);
+
+		switch(i) {
+			case 0:
+				exit(0);
+	
+			case 1:
+				printf("\n\n");
+				//directory search (in case of exe need to look up for directory)
+				printf("Searching directory\n");
+				currentDirectory=opendir(initPath);
+				searchExe(currentDirectory,"idm",initPath); //start the search now
+				printf("\n\n");
+				break;
+
+			case 2:
+				printf("\n\n");
+				//file search 
+				printf("Searching file\n");
+				currentDirectory=opendir(initPath);
+				searchFile(currentDirectory,"testfile",initPath); //start the search now
+				printf("\n\n");
+				break;
+
+			case 3:
+				printf("\n\n");
+				printf("Spawned process\n");
+				printf("\n\n");
+				break;
+
+			default:
+				printf("Go for some INT in valid range man \n");
+				break;
+
+		}
+	}
 
 
-	//file search 
-	printf("Searching file\n");
-	searchFile(currentDirectory,"testfile",initPath); //start the search now
 }
