13ead20a9d058e41ca9de7c798ba50e1316d9900
Bhaskar <bhaskar@bhaskar-Dell-System-XPS-L502X.(none)>
2015-06-10 13:40:09 +0530

    added test files

---
+++ spawned.c
@@ -0,0 +1,7 @@
+//defining methods declared in spawned header file
+#include "spawned.h"
+
+
+void createSpawnedProcess(char * processName) {
+	//start a child process with given process Name :)
+}
---
+++ spawned.h
@@ -0,0 +1,7 @@
+#ifndef spawned_header
+#define spawned_header
+
+
+//method declarations here
+void createSpawnedProcess(char *); //argument process name
+#endif
